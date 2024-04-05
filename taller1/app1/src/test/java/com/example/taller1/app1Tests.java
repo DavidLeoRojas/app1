139951afc2a794231c84c851a5e6e05be29e93e7
DavidLeoRojas <94474546+DavidLeoRojas@users.noreply.github.com>
2024-04-04 19:46:20 -0500

    Add files via upload

---
+++ taller1/app1/src/test/java/com/example/taller1/app1Tests.java
@@ -0,0 +1,13 @@
+package com.example.taller1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class app1Tests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
